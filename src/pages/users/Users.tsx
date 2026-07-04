faa9c866e463e27a823e77eb13dfd56ef227c233
dhruvnakum <dhruv.nakum25@gmail.com>
2026-07-04 16:31:03 -0700

    add users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,20 +1,64 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Skeleton, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
 const Users = () => {
+  const { data: users, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return getUsers().then((res) => res.data.data);
+    },
+  });
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
+      render: (_text: string, record) => {
+        return (
+          <div>
+            {record.firstName} {record.lastName}
+          </div>
+        );
+      },
+    },
+
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+    },
+  ];
+
   return (
     <>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[
-          {
-            title: <Link to="/">Dashboard</Link>,
-          },
-          {
-            title: "Users",
-          },
-        ]}
-      ></Breadcrumb>
+      <Space orientation="vertical" style={{ width: "100%" }} size="large">
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            {
+              title: <Link to="/">Dashboard</Link>,
+            },
+            {
+              title: "Users",
+            },
+          ]}
+        ></Breadcrumb>
+        {isLoading && <Skeleton active />}
+        <Table dataSource={users} columns={columns}></Table>
+      </Space>
     </>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: number;
+  email: string;
+  createdAt: string;
+  firstName: string;
+  lastName: string;
+};
